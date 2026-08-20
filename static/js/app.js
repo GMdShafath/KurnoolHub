1139e887eb94eeb0fa17f4bb60f147ca1aac4729
G Md Shafath <gshafath454@gmail.com>
2026-08-20 16:09:52 +0530

    Add All Categories dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -202,3 +202,12 @@ function nearMe() {
     );
 
 }
+function toggleCategoryDropdown() {
+    const menu = document.getElementById("categoryMenu");
+
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
